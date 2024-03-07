40a2ff8f4b5b287329b6b7271133d5b9a5c92e8b
tigerkaplane <husniyeerparun@gmail.com>
2024-03-07 01:26:20 +0000

    switch case

---
+++ app.js
@@ -0,0 +1,24 @@
+let number = prompt("Please enter a number between 1 to 5");
+
+switch (number) { // "1" "2" "3" "4" "5" // string
+  case "1":
+    // codes
+    console.log("Entered number is 1");
+    break;
+  case "2":
+    console.log("Entered number is 2");
+    break;
+  case "3":
+    console.log("Entered number is 3");
+    break;
+  case "4":
+    console.log("Entered number is 4");
+    break;
+  case "5":
+    console.log("Entered number is 5");
+    break;
+
+    default:
+    console.log("Please enter a number between 1 to 5");
+    break;
+}
